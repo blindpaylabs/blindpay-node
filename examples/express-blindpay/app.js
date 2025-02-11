863c1c46c6af8b377c78c7ead6e83b957860d894
Gabriel Miranda <mrarticuno@users.noreply.github.com>
2025-02-10 18:46:24 -0800

    Add Express.js example project with Blindpay integration

---
+++ examples/express-blindpay/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const { Blindpay } = require("blindpay");
+  const blindpay = new Blindpay("your-api-key");
+
+  blindpay.payouts
+    .retrievePayoutTrack("po_2H75fS3XtwCL")
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
