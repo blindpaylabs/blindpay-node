975c0d47d5abd609d226b92ee1663fc91dab8743
alvseven <alv7contato@outlook.com>
2025-11-18 21:26:51 -0300

    chore: add get payin quote method

--- src/resources/payins/quotes.ts
+++ src/resources/payins/quotes.ts
@@ -1,10 +1,12 @@
 import type { BlindpayApiResponse, Currency, CurrencyType, StablecoinToken } from "../../../types";
 import type { InternalApiClient } from "../../internal/api-client";
 
+export type PayinPaymentMethod = "ach" | "wire" | "pix" | "spei";
+
 export type CreatePayinQuoteInput = {
     blockchain_wallet_id: string;
     currency_type: CurrencyType;
-    payment_method: "ach" | "wire" | "pix" | "spei";
+    payment_method: PayinPaymentMethod;
     request_amount: number;
     token: StablecoinToken;
     cover_fees: boolean;
@@ -40,6 +42,35 @@ export type GetPayinFxRateResponse = {
     instance_percentage_fee: number;
 };
 
+export type GetPayinQuoteInput = string;
+
+export type GetPayinQuoteResponse = {
+    id: string;
+    payment_method: PayinPaymentMethod,
+    token: StablecoinToken;
+    request_amount: number;
+    cover_fees: boolean;
+    currency_type: CurrencyType;
+    expires_at: number | null;
+    currency: Extract<Currency, "BRL" | "USD" | "MXN" | "COP" | "ARS">;
+    commercial_quotation: number | null;
+    blindpay_quotation: number | null;
+    receiver_amount: number;
+    sender_amount: number;
+    partner_fee_amount: number | null;
+    flat_fee: number | null;
+    total_fee_amount: number | null;
+    payer_rules: {
+        pix_allowed_tax_ids: string[];
+    } | null;
+    blockchain_wallet_id: string;
+    instance_id: string;
+    partner_fee_id: string | null;
+    billing_fee: number | null;
+    created_at: string;
+    updated_at: string;
+};
+
 export function createPayinQuotesResource(instanceId: string, client: InternalApiClient) {
     return {
         create({
@@ -51,6 +82,9 @@ export function createPayinQuotesResource(instanceId: string, client: InternalAp
             ...data
         }: GetPayinFxRateInput): Promise<BlindpayApiResponse<GetPayinFxRateResponse>> {
             return client.post(`/instances/${instanceId}/payin-quotes/fx`, data);
+        },
+        get(id: GetPayinQuoteInput): Promise<BlindpayApiResponse<GetPayinQuoteResponse>> {
+            return client.get(`/instances/${instanceId}/payin-quotes/${id}`);
         },
     };
 }
